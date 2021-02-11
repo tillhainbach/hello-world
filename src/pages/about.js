24b292605aa15b968842acb1d34bb64aa5d069e4
Till Hainbach <till.hainbach@posteo.de>
2021-02-11 16:05:42 +0100

    init

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/header';
+
+const About = () => {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About Gatsby" />
+      <Header headerText="It's pretty cool" />
+      <p>such wow. Very React.</p>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
-import React from "react"
+import React from 'react';
+import { Link } from 'gatsby';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">contact</Link>
+      <h2>Hello Jana!</h2>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
 }
