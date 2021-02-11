bcfc8e86c5a6acfad269552b14f3293b8e5b2d3e
tillhainbach <tillhainbach@users.noreply.github.com>
2021-02-11 15:07:09 +0000

    deployed via Gatsby Publish Action 🎩 for 24b292605aa15b968842acb1d34bb64aa5d069e4

---
+++ component---src-pages-contact-js-dd448582ba580e9763b0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"Cuy+":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),c=n("Wbzz"),u=n("LbRr");t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{to:"/"},"Home"),r.a.createElement(u.a,{headerText:"Contact"}),r.a.createElement("p",null,"Send us a message!"))}},LbRr:function(e,t,n){"use strict";var a=n("q1tI"),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-dd448582ba580e9763b0.js.map
---
+++ component---src-pages-index-js-6204ecb115f47fcf751f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return r}));var a=n("q1tI"),c=n.n(a),l=n("Wbzz");function r(){return c.a.createElement("div",{style:{color:"purple"}},c.a.createElement(l.a,{to:"/contact/"},"contact"),c.a.createElement("h2",null,"Hello Jana!"),c.a.createElement("p",null,"What a world."),c.a.createElement("img",{src:"https://source.unsplash.com/random/400x200",alt:""}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-6204ecb115f47fcf751f.js.map
